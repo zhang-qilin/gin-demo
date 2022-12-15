4f4c6882ffb7c251eb299396a8b50eb3970eaa93
张齐林 <3323816129@qq.com>
2022-12-16 00:10:25 +0800

    文件服务

--- Gin_Base_01/then_Advanced/File_service.go
+++ Gin_Base_01/then_Advanced/File_service.go
@@ -5,3 +5,17 @@
 * @IDE ：GoLand
  */
 package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+	r.GET("/file", fileServer)
+	r.Run(":9090")
+}
+
+func fileServer(c *gin.Context) {
+	path := "D:/种子/github.com/gin-demo/"
+	fileName := path + c.Query("name")
+	c.File(fileName)
+}
