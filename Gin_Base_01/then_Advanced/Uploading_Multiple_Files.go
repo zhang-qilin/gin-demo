768db64b6fd899168611ffffe83ccceb9cb0f26f
张齐林 <3323816129@qq.com>
2022-12-17 00:09:27 +0800

    多文件上传

--- Gin_Base_01/then_Advanced/Uploading_Multiple_Files.go
+++ Gin_Base_01/then_Advanced/Uploading_Multiple_Files.go
@@ -5,3 +5,37 @@
 * @IDE ：GoLand
  */
 package main
+
+import (
+	"log"
+	"net/http"
+	
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	
+	r.POST("/upload", func(c *gin.Context) {
+		form, err := c.MultipartForm()  //  获取form
+		if err != nil {
+			c.String(http.StatusBadRequest,"文件上传错误：%s",err)
+		}
+		files := form.File["file_key"]  // 上传的所有文件
+		
+		// 定义文件上传后存放的位置
+		dst :="D:/种子/github.com/gin-demo/"
+		
+		// 遍历所有文件
+		for _, file :=range files{
+			c.SaveUploadedFile(file,dst + file.Filename)
+		}
+		c.String(http.StatusOK,"%d 个文件上传成功...",len(files))
+	})
+	
+	err := r.Run()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+}
