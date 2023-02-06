05c11f182b883bc9039e960510d995a1d872c947
张齐林 <3323816129@qq.com>
2023-02-06 19:38:49 +0800

    定义处理错误中间件

---
+++ Gin_Base_01/actualCombat(gin_applocation)/middleware/RecoveryMiddleware.go
@@ -0,0 +1,30 @@
+/*
+* @Time ： 2023-02-06 18:08
+* @Auth ： 张齐林
+* @File ：RecoveryMiddleware.go
+* @IDE ：GoLand
+ */
+
+package middleware
+
+// 处理错误中间件
+
+import (
+	"fmt"
+	"gin_applocation/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+func RecoverMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		defer func() {
+			// 主要是用于获取错误
+			if err := recover(); err != nil {
+				response.Fail(c, nil, fmt.Sprintf(err))
+				c.Abort()
+				return
+			}
+		}()
+	}
+}
